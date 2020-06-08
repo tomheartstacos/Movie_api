5eb293d5635d1cffa3467371cd7ba5d0d4debfa8
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-06-08 13:52:34 +0100

    Added express 250

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-// This file is a placeholder for now.
-// Nothing to see here. Please move along...
+const express = require('express');
+const app = express();
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my Movie club!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.use('/documentation', express.static('public'));
+});
+
+// use Morgan to log all requests
+
+const express = require('express'),
+  morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/movies', (req, res) => {
+  res.send('This is the movies folder.');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
+// error handling function
+
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  // logic
+});
