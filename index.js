8c03aaf18f508006690c567e21db41fed45c99c2
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-06-09 14:04:40 +0100

    Removed repetitive code

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
 
 // GET requests
 app.get('/', (req, res) => {
@@ -15,11 +19,6 @@ app.use('/documentation', express.static('public'));
 
 // use Morgan to log all requests
 
-const express = require('express'),
-  morgan = require('morgan');
-
-const app = express();
-
 app.use(morgan('common'));
 
 app.get('/', (req, res) => {
@@ -34,18 +33,6 @@ app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
 
-// error handling function
-
-const bodyParser = require('body-parser'),
-  methodOverride = require('method-override');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-
 app.use((err, req, res, next) => {
-  // logic
+
 });
